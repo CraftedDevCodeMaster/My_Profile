b945912f72650de9537735069e7e8bf8bb99547e
CraftedDevCodeMaster <praveenkumarr200025@gmail.com>
2025-04-26 17:43:40 +0530

    fix: vite server options and static serving

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import { registerRoutes } from "./routes.ts";
+
 
 const app = express();
 app.use(express.json());
